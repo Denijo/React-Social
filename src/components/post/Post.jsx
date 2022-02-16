38c96bbaa64e97f050b8402bc39c674e1b3a42fe
DeniJo <denijosuasamosir@gmail.com>
2022-02-16 10:45:36 +0700

    add rightbar

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -15,8 +15,20 @@ export default function Post() {
             <MoreVert />
           </div>
         </div>
-        <div className="postCenter"></div>
-        <div className="postButtom"></div>
+        <div className="postCenter">
+          <span className="postText">Hey It's my first Post</span>
+          <img src="/assets/post/1.jpeg" alt="" className="postImg" />
+        </div>
+        <div className="postButtom">
+          <div className="postBottomLeft">
+            <img src="/assets/like.png" alt="" className="likeIcon" />
+            <img src="/assets/heart.png" alt="" className="likeIcon" />
+            <span className="postLikeCounter">32 people like it</span>
+          </div>
+          <div className="postBottomRight">
+            <div className="postCommentText">9 Comments</div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,5 +1,75 @@
 import "./rightbar.css";
 
 export default function Rightbar() {
-  return <div className="rightbar">Rightbar</div>;
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img src="assets/gift.png" alt="" className="birthdayImg" />
+          <span className="birthdayText">
+            <b>Pola Foster</b> and <b>3 other friends</b> have a birthday today
+          </span>
+        </div>
+        <img src="/assets/ads.jpg" alt="" className="adsImage" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="assets/person/3.jpeg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Paul Tibbit</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="assets/person/3.jpeg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Paul Tibbit</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="assets/person/3.jpeg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Paul Tibbit</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="assets/person/3.jpeg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Paul Tibbit</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="assets/person/3.jpeg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Paul Tibbit</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
